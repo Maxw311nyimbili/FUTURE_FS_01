1d9a9fe12bcbaa1ecb2f1507ab1d269733d20366
Maxw311 <nyimbilimaxwell9@gmail.com>
2025-04-28 00:50:40 +0000

    Add path to .env file

--- send.py
+++ send.py
@@ -8,7 +8,9 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 # Load environment variables
-load_dotenv()
+
+project_folder = '/home/nyimbiliPortfolio/portfolio'
+load_dotenv(os.path.join(project_folder, '.env'))
 
 # Get email credentials from environment variables
 GMAIL_EMAIL = os.getenv("GMAIL_EMAIL")
